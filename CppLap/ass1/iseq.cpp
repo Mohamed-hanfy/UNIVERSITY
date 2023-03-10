b9d3778974e4089e454ccf5dec54bcfc9a84098d
Mohamed Hanafy <mohamed.hanfy.dev@outlook.com>
2023-03-10 14:17:01 +0200

    puch

---
+++ CppLap/ass1/iseq.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int x,y;
+ cout <<"Entre two numbers "<<"\n";
+ cin >>x>>y;
+ if(x==y){
+    cout <<x <<" is equal to "<<y<<"\n";
+ }
+ else if(x>y){
+     cout <<x <<" is lager than "<<y<<"\n";
+ }
+ else
+ cout <<y<<" is larger than "<<x<<"\n";
+ 
+}
